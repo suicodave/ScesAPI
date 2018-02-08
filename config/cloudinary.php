93d6cfd5edbf5be44e77d5980824aaa9576b5fd6
Always Hungry <suico.dave@gmail.com>
2018-02-08 23:46:56 +0800

    cloudinary env vars

--- config/cloudinary.php
+++ config/cloudinary.php
@@ -1,9 +1,11 @@
 <?php
-
+$name = getenv('CLOUD_NAME');
+$key = getenv('CLOUD_API_KEY');
+$secret = getenv('CLOUD_API_SECRET');
 return [
     'cloudinary' => [
-        'cloud_name' => env('CLOUD_NAME'),
-        'api_key' => env('CLOUD_API_KEY'),
-        'api_secret' => env('CLOUD_API_SECRET')
+        'cloud_name' => env('CLOUD_NAME', $name),
+        'api_key' => env('CLOUD_API_KEY', $key),
+        'api_secret' => env('CLOUD_API_SECRET', $secret)
     ]
 ];
